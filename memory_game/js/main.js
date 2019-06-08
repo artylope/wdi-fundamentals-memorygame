ae1b77c8c52eeecde19ecd172cedfe865b50dc10
Yi Xin Ang <angyixin@gmail.com>
2019-06-09 06:10:54 +0800

    converted logic into reuseable functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2){
+var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
-  } else if (cardsInPlay[0] !== cardsInPlay[1]) {
-    console.log("Sorry, try again.");
+  console.log("You found a match!");
   } else {
-
+    console.log("Sorry, try again.");
   }
 }
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if(cardsInPlay.length === 2){
+    checkForMatch();
+  }
+}
+
+flipCard(1);
+flipCard(3);
